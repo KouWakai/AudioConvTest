531ec0c8fc37ff6fd594cd32a1f6de29e680e737
KouWakai <kouyama112@gmail.com>
2021-04-05 21:25:40 +0900

    design done

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const electron = require("electron");
 const url = require('url');
 const Lame = require("node-lame").Lame;
 
-const {app, BrowserWindow, ipcMain, dialog} = electron;
+const {app, BrowserWindow, ipcMain, dialog, Menu} = electron;
 const nodepath = require('path');
 
 let { convname,filename} = '';
@@ -11,12 +11,13 @@ let mainWindow;
 
 app.on('ready', () => {
     mainWindow = new BrowserWindow({
+        width: 650,
+        height: 500,
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false
         }
     });
-
     // Load html into window
     mainWindow.loadURL(url.format({
         pathname: nodepath.join(__dirname, 'mainWindow.html'),
@@ -28,31 +29,37 @@ app.on('ready', () => {
         app.quit();
     });
 
-    // Catch Convert 
-    ipcMain.on('Convert',function(e){
-        // Replace wav to mp3 (WIP)
-        filename = filename.replace('wav', 'mp3');
-        // Set output location (path)
-        const output = "./audio-files/" + filename;
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
 
-        // Set target file by path
-        const encoder = new Lame({
-            output: output,
-            bitrate: 128,
-        }).setFile(convname);
+    Menu.setApplicationMenu(mainMenu);
+    mainWindow.removeMenu();
 
-        // Encode by node-lame
-        encoder.encode()
-            .then(() => {
-                // Encoding finished
-                console.log("success");
-            })
-            .catch((error) => {
-                // Something went wrong
-                console.log(error);
-            });
-    });
+});
 
+// Catch Convert
+ipcMain.on('Convert',function(e){
+  // Replace wav to mp3 (WIP)
+  filename = filename.replace('wav', 'mp3');
+  // Set output location (path)
+  const output = "./output/" + filename;
+
+  // Set target file by path
+  const encoder = new Lame({
+      output: output,
+      bitrate: 128,
+  }).setFile(convname);
+
+  // Encode by node-lame
+  encoder.encode()
+      .then(() => {
+          // Encoding finished
+          console.log("success");
+          e.reply('convert-reply', 'complete!')
+      })
+      .catch((error) => {
+          // Something went wrong
+          console.log(error);
+      });
 });
 
 ipcMain.handle('file-open', async (event) => {
@@ -89,3 +96,28 @@ ipcMain.handle('file-open', async (event) => {
       return({status:false, message:error.message});
     }
   });
+
+  const mainMenuTemplate = [
+    {
+      label : ' '
+    }
+  ]
+
+  // Add developer tools option if in dev
+if(process.env.NODE_ENV !== 'production'){
+  mainMenuTemplate.push({
+    label: 'Developer Tools',
+    submenu:[
+      {
+        role: 'reload'
+      },
+      {
+        label: 'Toggle DevTools',
+        accelerator:process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+        click(item, focusedWindow){
+          focusedWindow.toggleDevTools();
+        }
+      }
+    ]
+  });
+}
