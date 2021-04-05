288d5f023ffe22960cb48c86846b459a0cd97125
KLu11t <35868597+KLu11t@users.noreply.github.com>
2021-04-05 16:07:05 +0900

    change file opening function (input tag to dialog)

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const electron = require("electron");
 const url = require('url');
-const path = require('path');
 const Lame = require("node-lame").Lame;
 
-const {app, BrowserWindow, ipcMain} = electron;
+const {app, BrowserWindow, ipcMain, dialog} = electron;
+const nodepath = require('path');
+
+let { convname,filename} = '';
 
 let mainWindow;
 
@@ -17,7 +19,7 @@ app.on('ready', () => {
 
     // Load html into window
     mainWindow.loadURL(url.format({
-        pathname: path.join(__dirname, 'mainWindow.html'),
+        pathname: nodepath.join(__dirname, 'mainWindow.html'),
         protocol:'file:',
         slashes: true
     }));
@@ -27,13 +29,13 @@ app.on('ready', () => {
     });
 
     // Catch item:add
-    ipcMain.on('Convert',function(e, item, name){
-        name = name.replace('wav', 'mp3');
-        const output = "./audio-files/" + name;
+    ipcMain.on('Convert',function(e){
+        filename = filename.replace('wav', 'mp3');
+        const output = "./audio-files/" + filename;
         const encoder = new Lame({
             output: output,
             bitrate: 128,
-        }).setFile(item);
+        }).setFile(convname);
 
         encoder.encode()
             .then(() => {
@@ -48,3 +50,38 @@ app.on('ready', () => {
     });
 
 });
+
+ipcMain.handle('file-open', async (event) => {
+    // ファイルを選択
+    const paths = dialog.showOpenDialogSync(mainWindow, {
+      buttonLabel: '開く',  // 確認ボタンのラベル
+      filters: [
+        { name: 'Text', extensions: ['audiofiles', 'wav'] },
+      ],
+      properties:[
+        'openFile',         // ファイルの選択を許可
+        'createDirectory',  // ディレクトリの作成を許可 (macOS)
+      ]
+    });
+
+    // キャンセルで閉じた場合
+    if( paths === undefined ){
+      return({status: undefined});
+    }
+
+    // ファイルの内容を返却
+    try {
+      const path = paths[0];
+      convname = path;
+      filename = nodepath.basename(path);
+
+      return({
+        status: true,
+        path: path,
+        name: filename
+      });
+    }
+    catch(error) {
+      return({status:false, message:error.message});
+    }
+  });
